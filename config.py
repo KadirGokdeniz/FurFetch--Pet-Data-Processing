83e192cd2180826ebf014b90824355d634f33ca6
Kadir Gokdeniz <kadirqokdeniz@hotmail.com>
2025-04-27 22:20:39 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py - Yapılandırma ayarları
+CONFIG = {
+    "database": {
+        "host": "localhost",
+        "port": 3306,
+        "user": "root", 
+        "password": "",
+        "db_name": "mydatabase"
+    },
+    "scraping": {
+        "base_url": "https://www.petlebi.com/alisveris/ara",
+        "max_pages": 222
+    }
+}
